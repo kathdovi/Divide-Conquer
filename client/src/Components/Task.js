d1d20019977940af46f3c9c6b2a47b24ec858073
Amy Lanclos <ajl275@cornell.edu>
2019-03-23 14:18:05 -0400

    began on Task.js

--- client/src/Components/Task.js
+++ client/src/Components/Task.js
@@ -1,18 +1,31 @@
 // TODO: Make a single task here. Will also render its subtasks
 // Uncomment the code & write the jsx
-
+/*
 import React, { Component } from "react";
 import Subtask from "./Subtask";
 
+let nextTaskId = 0;
 
 class Task extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {done: false};
+		this.state = {id: nextTaskId};
+		nextTaskId += 1;
+	}
+	
+	toggle() {
+		this.setState(!this.done);
+	}
 
     render() {
 
         return (
-            // jsx goes here
+            
         );
     }
 }
 
 export default Task;
+*/
