c48f73f8ca69edecb380c6529e7ac5b770f98b35
Amy Lanclos <ajl275@cornell.edu>
2019-03-23 21:53:42 -0400

    deletion and insertion works!

---
+++ client/src/Components/TaskList.js
@@ -0,0 +1,56 @@
+// TODO: Make a task list here. Will render tasks.
+// Uncomment the code & write the jsx
+
+import React, { Component } from "react";
+import Task from "./Task";
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import Container from "react-bootstrap/Container";
+
+
+class TaskList extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {tasks : []};
+		this.addTask = this.addTask.bind(this);
+		this.handleNextTaskChange = this.handleNextTaskChange.bind(this)
+		this.remove = this.remove.bind(this);
+	}
+
+	remove(e) {
+		const newtasks = this.state.tasks.filter(task => task.value != e)
+		this.setState({tasks : newtasks});
+	}
+	
+    render() {
+
+        return (
+            <Container> 
+			<div>{this.state.tasks.map(task => <Task value={task.value} key={task.value} fun={()=>this.remove(task.value)} />)}</div>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <Button onClick={this.addTask} variant="outline-secondary" style={{"fontSize": "20px", "width": "70px"}}> + </Button>
+                    </InputGroup.Prepend>
+                    <FormControl style={{ "height": "60px", "fontSize": "20px" }} aria-describedby="basic-addon1" value={this.state.nextTaskValue} onChange={this.handleNextTaskChange} />
+                </InputGroup>
+            </Container>
+        );
+    }
+	
+	addTask(e) {
+		const newtask = {value : this.state.nextTaskValue};
+		this.setState({tasks : this.state.tasks.concat([newtask])});
+	}
+	
+	handleNextTaskChange(e) {
+		const nextTaskValue = e.target.value
+		this.setState({
+			nextTaskValue
+		})
+	}
+	
+}
+
+export default TaskList;
