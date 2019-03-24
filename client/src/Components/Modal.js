6ab3ffd012458c98079b6382dcc2a660785f088c
margotjohnson <mardenjohnson+smartGit@gmail.com>
2019-03-23 20:13:45 -0400

    connect modal to datapbase results and randomize reinforcement message output

--- client/src/Components/Modal.js
+++ client/src/Components/Modal.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
+import axios from 'axios'
 
 let message = "placeholder";
+let rand = Math.floor(Math.random() * 10) +1
 
 // get score from the api here
 // Add the get request
-// axios.get().then(response => console.log(response));
 // parse response and set message to the response
 
 
 class PositiveModal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ""
+        }
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:8080/reinforcement-call.jsp?aff_id=" + rand)
+        .then(response => {
+            console.log(rand)
+            const message = response.data.message;
+            this.setState({message});
+            });
+    }
     render() {
         return (
             <Modal
@@ -21,11 +37,11 @@ class PositiveModal extends React.Component {
             >
                 <Modal.Header closeButton>
                     <Modal.Title id="contained-modal-title-vcenter">
-                        You Did It!
+                        Task Completed!
           </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h4> {message} </h4>
+                    <h4> {this.state.message} </h4>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={this.props.onHide}>Close</Button>
